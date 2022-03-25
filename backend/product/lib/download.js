b2502ba0402db14deccf485a00f1c70e04610ec3
Derek David <derekmiracledavid@hotmail.com>
2022-03-25 16:05:29 +0100

    resolved security risks

--- backend/product/lib/download.js
+++ backend/product/lib/download.js
@@ -1,16 +1,16 @@
 import AWS from "aws-sdk";
 import fs from "fs";
 
-const bucketName = "shopit-products";
-//k
-const S3 = new AWS.S3({
-  region: "us-east-1",
-  accessKeyId: "AKIA3RE6BQWPRP47AHKS",
-  secretAccessKey: "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",
-  Bucket: bucketName,
-});
-
 export default function download(fileKey) {
+  const bucketName = "shopit-products";
+  //k
+  const S3 = new AWS.S3({
+    region: "us-east-1",
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_KEY,
+    Bucket: bucketName,
+  });
+  console.log("kr", process.env.AWS_SECRET_KEY, process.env.AWS_ACCESS_KEY_ID);
   return S3.getObject({
     Key: fileKey,
     Bucket: bucketName,
