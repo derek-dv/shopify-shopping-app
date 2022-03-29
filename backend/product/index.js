51888ffe14e46921cedb708234e79f4e6c9c3479
Derek David <derekmiracledavid@hotmail.com>
2022-03-29 13:15:32 +0100

    produc service change

--- backend/product/index.js
+++ backend/product/index.js
@@ -13,19 +13,19 @@ const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env;
 console.log(DB_USER, DB_PASSWORD);
 
 mongoose
-  .connect(`mongodb://mongodb/products`, {
+  .connect(`mongodb://product_mongodb`, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
   })
   .then(() => {
-    app.use("/api/products", productRouter);
+    app.use("/api", productRouter);
 
     app.get("/", (req, res) => {
       res.send("products is ready");
     });
 
-    const port = process.env.NODE_DOCKER_PORT || 5000;
+    const port = 6868// process.env.NODE_DOCKER_PORT || 5000;
     app.listen(port, () => {
       console.log(`listen at localhost:${port}`);
     });
